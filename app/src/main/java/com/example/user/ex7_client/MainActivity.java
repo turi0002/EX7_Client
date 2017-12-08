fceff9aaa25a933927c8d26a0e8aab677b8d7375
turi0002 <trui0002@gmail.com>
2017-12-08 11:34:28 +0200

    asd

--- app/src/main/java/com/example/user/ex7_client/MainActivity.java
+++ app/src/main/java/com/example/user/ex7_client/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.user.ex7_client;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+    private final int MY_CALL_REQUEST =1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +25,14 @@ public class MainActivity extends Activity {
         switch (view.getId()) {
             case R.id.bCall:
                 String number = ((EditText)findViewById(R.id.edCall)).getText().toString();
-                callPhone(number);
+                if(checkSelfPermission(Manifest.permission.CALL_PHONE)== PackageManager.PERMISSION_GRANTED){
+                    callPhone(number);
+                }
+                else{
+                    requestPermissions(new String[]{Manifest.permission.CALL_PHONE},MY_CALL_REQUEST);
+                }
+
+
                 break;
             case R.id.bsurf:
                 break;
@@ -34,9 +45,23 @@ public class MainActivity extends Activity {
         private void callPhone(String phoneNumber){
 
             Uri call = Uri.parse("tel:" + phoneNumber);
-            Intent surf = new Intent(Intent.ACTION_DIAL, call);
+            Intent surf = new Intent(Intent.ACTION_CALL, call);
             startActivity(surf);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode)
+        {
+            case MY_CALL_REQUEST:
+                if      (grantResults.length>0&&
+                         permissions[0].equals(Manifest.permission.CALL_PHONE)&&
+                        ( grantResults[0]==PackageManager.PERMISSION_GRANTED)){
+                    ((EditText)findViewById(R.id.edCall)).getText().toString();
+                }
+        }
     }
+}
 
 
